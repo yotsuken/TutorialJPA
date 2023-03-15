f107c8631b3291b3a0402668550a6e00346f04f2
yotsuken <yotsumoto.ken@gmail.com>
2023-03-16 07:03:05 +0900

    ２課題前コミット

--- src/main/java/com/techacademy/CountryService.java
+++ src/main/java/com/techacademy/CountryService.java
@@ -1,6 +1,8 @@
 package com.techacademy;
 
 import java.util.List;
+import java.util.Optional; // 追加
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,4 +21,28 @@ public class CountryService {
         // リポジトリのfindAllメソッドを呼び出す
         return repository.findAll();
     }
+
+    // ----- 追加:ここから -----
+    // 1件を検索して返す
+    public Country getCountry(String code) {
+        // findByIdで検索
+        Optional<Country> option = repository.findById(code);
+        // 取得できなかった場合はnullを返す
+        Country country = option.orElse(null);
+        return country;
+    }
+
+    // 更新（追加）を行なう
+    @Transactional
+    public void updateCountry(String code, String name, int population) {
+        Country country = new Country(code, name, population);
+        repository.save(country);
+    }
+
+    // 削除を行なう
+    @Transactional
+    public void deleteCountry(String code) {
+        repository.deleteById(code);
+    }
+    // ----- 追加:ここまで -----
 }
